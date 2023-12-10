a4b114f45ca82b47b7d2b09ab872b6c00e2fbf5c
Aromia <f2940015504@gmail.com>
2023-12-10 20:52:16 +0800

    feat: add an option to forever disable sleep

--- usr/scripts/fsleep.zsh
+++ usr/scripts/fsleep.zsh
@@ -5,16 +5,19 @@ fsleep()
 {
     sudo pmset disablesleep 1
 
-    if [ $# = 0 ];then
-        time=$(( 10 * 60 ))
+    if [ "$1" = "f" ];then
+    echo "Sleep will be disabled forever. You can interupt with enter key. "
+    read
     else
-        time=$(( $1 * 60 ))
-        time=$(( int($time) ))
-    fi
-
-    date -r $(( $(date +%s) + $time )) +'Sleep will be disabled until %H:%M:%S. You can interupt by pressing enter ...'
-
+        if [ $# = 0 ];then
+            time=$(( 10 * 60 ))
+        else
+            time=$(( $1 * 60 ))
+            time=$(( int($time) ))
+        fi
+    date -r $(( $(date +%s) + $time )) +'Sleep will be disabled until %H:%M:%S. You can interupt wieh enter key ...'
     read -t $time
+    fi
 
     sudo pmset disablesleep 0
 }
