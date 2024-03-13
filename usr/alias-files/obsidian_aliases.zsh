c1e2cbb4d73a95b790391e148b9af41d809fd1b0
Aromia <f2940015504@gmail.com>
2024-03-13 19:11:24 +0800

    add one obsidian alias

--- usr/alias-files/obsidian_aliases.zsh
+++ usr/alias-files/obsidian_aliases.zsh
@@ -1,3 +1,4 @@
 alias learn="open 'obsidian://open?vault=LearningMaterial'"
 alias note="open 'obsidian://open?vault=Aromia_Notebook'"
+alias diary="open 'obsidian://open?vault=diary'"
 alias obsDev="open 'obsidian://open?vault=Dev_Notebook'"
