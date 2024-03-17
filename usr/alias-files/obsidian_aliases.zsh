2011f3208451a59a622e2a8ee1e7acd6b738975b
Aromia <f2940015504@gmail.com>
2024-03-18 01:05:01 +0800

    change cd directory of diary

--- usr/alias-files/obsidian_aliases.zsh
+++ usr/alias-files/obsidian_aliases.zsh
@@ -1,4 +1,4 @@
 alias learn="open 'obsidian://open?vault=LearningMaterial'"
 alias note="open 'obsidian://open?vault=Aromia_Notebook'"
-alias diary="open 'obsidian://open?vault=diary'; cd /Users/aromia/diary-git/diary"
+alias diary="open 'obsidian://open?vault=diary'; cd /Users/aromia/diary-git"
 alias obsDev="open 'obsidian://open?vault=Dev_Notebook'"
