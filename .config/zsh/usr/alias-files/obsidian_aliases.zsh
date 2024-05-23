4f8517b0d903ca979d11488130e1f8293bfcc027
Aromia <f2940015504@gmail.com>
2024-05-23 20:04:15 +0800

    chore: update tmux and alias configs

--- .config/zsh/usr/alias-files/obsidian_aliases.zsh
+++ .config/zsh/usr/alias-files/obsidian_aliases.zsh
@@ -1,3 +1,6 @@
+if [ "$(uname -m)" = "x86_64" ]; then
+  alias obsidian="obsidian --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime"
+fi
 alias learn="open 'obsidian://open?vault=LearningMaterial'"
 alias note="open 'obsidian://open?vault=Aromia_Notebook'"
 alias diary="open 'obsidian://open?vault=diary'; cd /Users/aromia/diary-git"
