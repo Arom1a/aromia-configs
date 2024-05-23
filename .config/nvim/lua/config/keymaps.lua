c16407f493d045027ce1d48c257a4d288c2ad13a
Aromia <f2940015504@gmail.com>
2024-05-23 21:51:02 +0800

    chore: c-p in nvim & arch alias

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Telescope find files" })
+map("i", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Telescope find files" })
