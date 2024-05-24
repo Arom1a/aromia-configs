31fbc981777534efeffdf00693767298d32a591e
Aromia <f2940015504@gmail.com>
2024-05-25 00:13:48 +0800

    chore: auto download mason plugins

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "astro-language-server",
+        "black",
+        "clang-format",
+        "clangd",
+        "cpptools",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "isort",
+        "json-lsp",
+        "lua-language-server",
+        "markdown-oxide",
+        "prettierd",
+        "python-lsp-server",
+        "rust-analyzer",
+        "shfmt",
+        "stylua",
+        "tailwindcss-language-server",
+        "yaml-language-server",
+        "yamlfmt",
+      },
+    },
+  },
+}
