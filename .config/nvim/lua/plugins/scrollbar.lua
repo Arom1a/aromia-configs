f5168ecb6b26bc1a644a9a7d57446007378e4181
Aromia <f2940015504@gmail.com>
2024-05-29 12:19:08 +0800

    chore: add scrollbar

---
+++ .config/nvim/lua/plugins/scrollbar.lua
@@ -0,0 +1,8 @@
+return {
+  "petertriho/nvim-scrollbar",
+  config = function()
+    require("gitsigns").setup()
+    require("scrollbar.handlers.gitsigns").setup()
+    require("scrollbar").setup()
+  end,
+}
