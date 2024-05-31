372e714f59dfbcfc30aa8afd7706f0e8e2c0e3cf
Aromia <f2940015504@gmail.com>
2024-05-31 09:58:40 +0800

    fix: change obsidian vault aliase

--- .config/zsh/usr/alias-files/obsidian_aliases.zsh
+++ .config/zsh/usr/alias-files/obsidian_aliases.zsh
@@ -2,6 +2,6 @@ if [ "$(uname -m)" = "x86_64" ]; then
   alias obsidian="obsidian --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime"
 fi
 alias learn="open 'obsidian://open?vault=LearningMaterial'"
-alias note="open 'obsidian://open?vault=Aromia_Notebook'"
+alias note="open 'obsidian://open?vault=notebooks'"
 alias diary="open 'obsidian://open?vault=diary'; cd /Users/aromia/diary-git"
 alias obsDev="open 'obsidian://open?vault=Dev_Notebook'"
