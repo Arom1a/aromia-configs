c230ca5fe2af88f360ca95159a27fa06a3a74814
Aromia <f2940015504@gmail.com>
2024-06-19 04:00:28 +0800

    feat: add more formatter

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_bt_ft = {
+        html = { "prettierd" },
+        astro = { "prettierd" },
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+      },
+    },
+  },
+}
