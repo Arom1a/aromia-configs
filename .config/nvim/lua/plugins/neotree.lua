a1bff7c38c05c8395284274fabfd4550e7f4c2d2
Aromia <f2940015504@gmail.com>
2024-07-09 11:39:24 -0400

    feat: neovim display dot files

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -6,6 +6,13 @@ return {
         position = "right",
         width = "30",
       },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+      },
     },
   },
 }
