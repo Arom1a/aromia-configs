ccd4e71677595da983a9725ad2ebdbc90375cbfb
Aromia <f2940015504@gmail.com>
2025-01-31 11:57:25 +0800

    chore: nvim del redundant config

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -206,16 +206,6 @@ return {
     end,
   },
 
-  -- mdx syntax highlighting
-  {
-    "davidmh/mdx.nvim",
-    event = "BufEnter *.mdx",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("mdx").setup()
-    end,
-  },
-
   -- project management
   {
     "folke/persistence.nvim",
