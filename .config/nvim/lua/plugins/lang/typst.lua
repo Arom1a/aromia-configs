16619ab23181c455c6b65a2834a25f2e5f4cb7da
Aromia <f2940015504@gmail.com>
2026-02-10 18:41:37 -0500

    feat(nvim): add opts for typst preview

--- .config/nvim/lua/plugins/lang/typst.lua
+++ .config/nvim/lua/plugins/lang/typst.lua
@@ -3,6 +3,9 @@ return {
     "chomosuke/typst-preview.nvim",
     ft = "typst",
     opts = {
+      port = 40585,
+      invert_colors = '{"rest": "always","image": "never"}',
+      follow_cursor = false,
       dependencies_bin = { ["tinymist"] = "tinymist" },
     },
   },
